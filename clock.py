dcd96b7dd2513632257629933b84b27af219acc7
nathanweinberg <nathan2@stwmd.net>
2018-03-15 14:48:43 -0400

    Testing scheduled message sending by bot

---
+++ clock.py
@@ -0,0 +1,11 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from app import send_message
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('cron', day_of_week='mon', hour=24)
+def kitchen_reminder():
+    msg = "This is a test kitchen reminder! Soon I will tag whose week it is!"
+    send_message(msg)
+
+sched.start()
