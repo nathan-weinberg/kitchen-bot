2af1eeefd455fdf4b137a64c80f5a4a638675e6d
nathanweinberg <nathan2@stwmd.net>
2018-03-15 01:02:59 -0400

    Cleaned up now-functional code

--- app.py
+++ app.py
@@ -2,16 +2,19 @@ import os
 import sys
 import json
 import requests
-
 from flask import Flask, request
 
 app = Flask(__name__)
 
+# routes case to app when message to GroupMe is sent
 @app.route('/', methods=['POST'])
 def webhook():
-	data = request.get_json()
-	log('Recieved {}'.format(data))
 
+	# decode JSON data
+	data = request.get_json()
+	log('Received {}'.format(data))
+
+	# prevent bot from quoting itself
 	if data['name'] != 'KitchenBot':
 		msg = '{}, you sent "{}".'.format(data['name'], data['text'])
 		send_message(msg)
@@ -19,10 +22,11 @@ def webhook():
 	return "ok", 200
 
 def send_message(msg):
+	# GroupMe API
 	url = 'https://api.groupme.com/v3/bots/post'
-
+	# HTTP Post Body
 	data = {'bot_id': os.getenv('GROUPME_BOT_ID'), 'text': msg}
-
+	# HTTP Post 
 	resp = requests.post(url, json=data)
 
 def log(msg):
