b9c6f5f905e2c38897c6d801af0995a5ba4ce27e
nathanweinberg <nathan2@stwmd.net>
2018-03-14 23:36:03 -0400

    Testing changes

--- app.py
+++ app.py
@@ -20,10 +20,7 @@ def webhook():
 def send_message(msg):
 	url = 'https://api.groupme.com/v3/bots/post'
 
-	data = {
-			'bot_id' : os.getenv('GROUPME_BOT_ID'),
-			'text'   : msg,
-			}
+	data = {'bot_id':os.getenv('GROUPME_BOT_ID'), 'text':msg}
 
 	request = Request(url, urlencode(data).encode())
 	json = urlopen(request).read().decode()
