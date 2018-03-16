30429fce65e90699617b819978da79872327d33b
nathanweinberg <nathan2@stwmd.net>
2018-03-15 22:53:12 -0400

    Altered clock test

--- clock.py
+++ clock.py
@@ -3,7 +3,7 @@ from app import send_message
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon', hour=23)
+@sched.scheduled_job('cron', day_of_week='fri', hour=0)
 def kitchen_reminder():
     msg = "This is a test kitchen reminder! Soon I will tag whose week it is!"
     send_message(msg)
